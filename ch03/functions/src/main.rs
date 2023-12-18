18f9e2f60492db0649566d1612d6dff6c7d7ad27
Kasra Safari <kasrasafari@gmail.com>
2023-12-18 00:05:30 -0500

    chapter 3

---
+++ ch03/functions/src/main.rs
@@ -0,0 +1,13 @@
+fn five() -> i32 {
+    5
+}
+
+fn main() {
+    let x = five();
+    
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
